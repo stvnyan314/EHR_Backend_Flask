4c0f9f437a3a2615d6bc22f42d85ee2e953da8f6
Steven Yan <stvnyan314@yahoo.com>
2021-07-26 13:13:27 -0700

    add swagger ui and docker containers now connect

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # EHR_Backend
+
+to access flask app
+http://localhost:5000
+
+to access mongodb
+sudo docker exec -it mongodb /bin/bash
--- api/app.py
+++ api/app.py
@@ -1,28 +1,45 @@
 from flask import Flask, jsonify
 import pymongo
 from pymongo import MongoClient
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 
+
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Seans-Python-Flask-REST-Boilerplate"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
+
+
+
 def get_db():
-    client = MongoClient(host='test_mongodb',
-                         port=27017, 
-                         username='root', 
-                         password='pass',
-                        authSource="admin")
-    db = client["animal_db"]
+    client = MongoClient('mongodb://mongodb:27017')
+    db = client["food"]
     return db
 
 @app.route('/')
 def ping_server():
     return "Welcome to the world of animals."
 
-@app.route('/animals')
-def get_stored_animals():
+@app.route('/food')
+def get_stored_food():
     db = get_db()
-    _animals = db.animal_tb.find()
-    animals = [{"id": animal["id"], "name": animal["name"], "type": animal["type"]} for animal in _animals]
-    return jsonify({"animals": animals})
+    _fruits = db.fruits.find()
+    fruits = [{
+    "name": fruit["name"], 
+    "origin": fruit["origin"], 
+    "price": fruit["price"]
+    } for fruit in _fruits]
+    return jsonify({"fruits": fruits})
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000)
