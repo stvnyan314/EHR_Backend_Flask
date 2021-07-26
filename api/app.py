6b7580701e20ebb60402304ba41795cd8b53acd8
Steven Yan <stvnyan314@yahoo.com>
2021-07-26 16:31:16 -0700

    add parameter to flask get

--- api/app.py
+++ api/app.py
@@ -1,22 +1,25 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_restx import Api, Resource
 import pymongo
 from pymongo import MongoClient
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(app, version='1.0', title='Electronic Health Records',
+    description='A simple api for EHR')
+
+ns = api.namespace('EHR', description='Electronic Health Records')
 
 def get_db(database):
     client = MongoClient('mongodb://mongodb:27017')
     db = client[database]
     return db
 
-@api.route('/hello')
+@ns.route('/hello')
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
-@api.route('/patients')
+@ns.route('/patients')
 class GetPatients(Resource):
     def get(self):
         db = get_db("patients")
@@ -27,6 +30,21 @@ class GetPatients(Resource):
             "sex":patient["sex"]
         } for patient in _patients]
         return jsonify({"patients": patients})
+
+
+@ns.route('/patient/<int:id>')
+class GetPatients(Resource):
+    def get(self, id):
+        db = get_db("patients")
+        _patients = db.patients.find()
+        patients = [{
+            "name":patient["name"],
+            "dob":patient["dob"],
+            "sex":patient["sex"]
+        } for patient in _patients]
+        return jsonify({"patients": patients[id]})
+
+
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000)
