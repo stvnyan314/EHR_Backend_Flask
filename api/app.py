b3822aaf10871a1ac27a431e944b0dd0ce0a59f6
Steven Yan <stvnyan314@yahoo.com>
2021-07-26 15:08:39 -0700

    add flask restx to automatically add swagger ui

--- api/app.py
+++ api/app.py
@@ -1,45 +1,32 @@
 from flask import Flask, jsonify
+from flask_restx import Api, Resource
 import pymongo
 from pymongo import MongoClient
-from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
-
-
-### swagger specific ###
-SWAGGER_URL = '/swagger'
-API_URL = '/static/swagger.json'
-SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "Seans-Python-Flask-REST-Boilerplate"
-    }
-)
-app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
-### end swagger specific ###
-
-
+api = Api(app)
 
 def get_db(database):
     client = MongoClient('mongodb://mongodb:27017')
     db = client[database]
     return db
 
-@app.route('/')
-def ping_server():
-    return "Welcome to the world of animals."
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
-@app.route('/patients')
-def get_patients_info():
-    db = get_db("patients")
-    _patients = db.patients.find()
-    patients = [{
-        "name":patient["name"],
-        "dob":patient["dob"],
-        "sex":patient["sex"]
-    } for patient in _patients]
-    return jsonify({"patients": patients})
+@api.route('/patients')
+class GetPatients(Resource):
+    def get(self):
+        db = get_db("patients")
+        _patients = db.patients.find()
+        patients = [{
+            "name":patient["name"],
+            "dob":patient["dob"],
+            "sex":patient["sex"]
+        } for patient in _patients]
+        return jsonify({"patients": patients})
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000)
