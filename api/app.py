df25c311fcb46f371533bc72dc4bd5dffcae1ed6
Steven Yan <stvnyan314@yahoo.com>
2021-07-28 17:02:53 -0700

    add new doc to db

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from flask_restx import Api, Resource
+from flask_restx import Api, Resource, fields
 import pymongo
 from pymongo import MongoClient
 
@@ -9,26 +9,36 @@ api = Api(app, version='1.0', title='Electronic Health Records',
 
 ns = api.namespace('EHR', description='Electronic Health Records')
 
+patient_model = ns.model('Patient Model', 
+                {
+                    'first_name': fields.String(required = False,
+                        example = "John",
+                        description="First name of the person", 
+                        help="Name cannot be blank."),
+                    'middle_name': fields.String(required = False, 
+                        description="First name of the person"),
+                    'last_name': fields.String(required = False, 
+                        example = "Doe",
+                        description="Last name of the person", 
+                        help="Name cannot be blank."),
+                    'dob': fields.String(required = False,
+                        description="Date of birth",
+                        help="DOB cannot be blank."),
+                    'sex': fields.String(required = False,
+                        description="Sex of person")
+                })
+
 def get_db(database):
     client = MongoClient('mongodb://mongodb:27017')
     db = client[database]
     return db
 
-@ns.route('/hello')
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
 @ns.route('/patients')
 class GetPatients(Resource):
     def get(self):
         db = get_db("patients")
-        _patients = db.patients.find()
-        patients = [{
-            "name":patient["name"],
-            "dob":patient["dob"],
-            "sex":patient["sex"]
-        } for patient in _patients]
+        _patients = db.patients.find({},{ "_id": 0})
+        patients = [patient for patient in _patients]
         return jsonify({"patients": patients})
 
 
@@ -36,14 +46,28 @@ class GetPatients(Resource):
 class GetPatients(Resource):
     def get(self, id):
         db = get_db("patients")
-        _patients = db.patients.find()
-        patients = [{
-            "name":patient["name"],
-            "dob":patient["dob"],
-            "sex":patient["sex"]
-        } for patient in _patients]
+        _patients = db.patients.find({},{ "_id": 0})
+        patients = [patient for patient in _patients]
         return jsonify({"patients": patients[id]})
 
+@ns.route('/newpatient')
+@ns.expect(patient_model)
+class AddPatient(Resource):
+    def post(self):
+        db = get_db("patients")
+        db.patients.insert_one(request.json)
+        return {"status": db.patients.count()}
+
+@ns.route('/findpatient')
+@ns.expect(patient_model)
+class FindPatients(Resource):
+    def put(self):
+        db = get_db("patients")
+        patient_query = request.json
+        _patients = db.patient.find(patient_query)
+        patients = [patient for patient in _patients]
+        print(patient_query)
+        return {"matching patient": patients}
 
 
 if __name__=='__main__':
